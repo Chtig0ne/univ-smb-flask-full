2f43d6819cb438db9c4c6de97f1dbf7b99a2d912
ben pey <chtigone1@gmail.com>
2023-03-24 10:47:19 +0000

    v2 du site

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,20 +1,7 @@
 from flask import Flask
-from flask import render_templates
 
 app = Flask(__name__)
 
-@app.route("/home")
-def home():
-    return render_templates('home.html')
-
-@app.route('/about')
-def about():
-    return render_templates('about.html')
-
-@app.route('/services')
-def services():
-    return render_templates('services.html')
-
-@app.route('/contact')
-def contact():
-    return render_templates('contact.html')  
+@app.route("/")
+def hello():
+    return "Hello, API!"
--- src/flask-website/website.py
+++ src/flask-website/website.py
@@ -4,5 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def start(): 
-    return render_template('start.html')
-    
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
