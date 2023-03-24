aa3f3b7da7448ecfa83bfe59b65d5b4a5f61a54c
ben pey <chtigone1@gmail.com>
2023-03-24 10:27:28 +0000

    v1 du site

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,7 +1,20 @@
 from flask import Flask
+from flask import render_templates
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello, API!"
+@app.route("/home")
+def home():
+    return render_templates('home.html')
+
+@app.route('/about')
+def about():
+    return render_templates('about.html')
+
+@app.route('/services')
+def services():
+    return render_templates('services.html')
+
+@app.route('/contact')
+def contact():
+    return render_templates('contact.html')  
