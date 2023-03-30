e1c1d06ec6b000efd1c2803e68990fd0e47493fc
ben pey <chtigone1@gmail.com>
2023-03-30 13:13:36 +0000

    get_table

--- src/flask-api/api.py
+++ src/flask-api/api.py
@@ -1,5 +1,6 @@
-from flask import Flask,request
+from flask import Flask,render_template
 import mydb.py
+import mysql.connector
 
 app = Flask(__name__)
 
@@ -7,9 +8,25 @@ app = Flask(__name__)
 def hello():
     return "Hello, API!"
 
-@app.route("/")
-def get_():
-    return
+@app.route("/index.html")
+ # Connexion à la base de données
+db = mysql.connector.connect(
+    host="localhost",
+    user="yourusername",
+    password="yourpassword",
+    database="yourdatabase"
+)
+    
+# Exécution de la requête pour récupérer les enregistrements de la base de données
+cursor = db.cursor()
+cursor.execute("SELECT * FROM users")
+users = cursor.fetchall()
+
+# Fermeture de la connexion à la base de données
+db.close()
+
+# Affichage des enregistrements dans une page HTML
+return render_template('index.html') 
 
 
 
